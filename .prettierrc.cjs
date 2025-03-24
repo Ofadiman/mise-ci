734616247bd4251b293d5978caf958a887672364
ofadiman <ofadiman@gmail.com>
2025-03-24 22:24:16 +0100

    chore: install toml prettier plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,9 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
 module.exports = {
+  plugins: ['prettier-plugin-toml'],
   arrowParens: 'always',
   printWidth: 120,
   quoteProps: 'as-needed',
